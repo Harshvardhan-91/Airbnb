76c9a73642d16b7291505e3507298a6f36e071ab
Harshvardhan-91 <agarwalharshvardhan081@gmail.com>
2025-01-01 14:44:58 +0530

    completed navbar

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,4 +1,7 @@
 import Container from "../Container";
+import Logo from "./Logo";
+import Search from "./Search";
+import UserMenu from "./UserMenu";
 
 const Navbar = () => {
   return (
@@ -6,9 +9,10 @@ const Navbar = () => {
         <div className="py-4 border-b-[1px]">
             <Container>
                 <div className="flex flex-row items-center justify-between gap-3 md:gap-0">
-
+                  <Logo/>
+                  <Search/>
+                  <UserMenu/>
                 </div>
-
             </Container>
 
         </div>
